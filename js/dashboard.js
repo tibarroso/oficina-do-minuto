084d9d85a1d2d5432a3b5c1a93d7af20631584f3
tibarroso <ti@ebarroso.com.br>
2026-01-27 15:30:15 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -30,6 +30,12 @@ async function verificarLogin() {
 <!-- Botão para criar pedidos -->
 <button id="btnCriarPedido">Criar Pedido</button>
 
+
+// Redireciona para pedidos.html
+document.getElementById("btnCriarPedido").addEventListener("click", () => {
+  window.location.href = "pedidos.html";
+});
+
 // ===============================
 // Carregar pedidos
 // ===============================

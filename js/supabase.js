60008533df1100993fbb164dbe32f69d0990cbec
tibarroso <ti@ebarroso.com.br>
2026-09-11 14:45:31 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,11 +1,14 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-// URL do seu projeto Supabase
+// Configurações do Projeto Supabase
 const SUPABASE_URL = "https://otplwkqekzihwvjwdezz.supabase.co";
-
-// Chave pública ANON (para uso seguro no front-end)
-// ⚠️ Nunca use a chave `service_role` no front-end
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90cGx3a3Fla3ppaHd2andkZXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1MDE5NTUsImV4cCI6MjA4NTA3Nzk1NX0.XymAmVbOUtLwOuXbHkHM3GaDNyE_6mDa8UW4DE1mQbw";
 
-// Cria cliente Supabase para front-end
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Inicialização do cliente Supabase com persistência local ativa
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+});
